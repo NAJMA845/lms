2b8a43b65f9af262489e2c3a050a75ccdef50a2d
M.M.F.Najma <s92082845@ousl.lk>
2024-09-16 11:05:45 +0530

    Database creation

---
+++ admin/config/database.php
@@ -0,0 +1,15 @@
+<?php
+// Create a connection using MySQLli
+$servername = "localhost";
+$username = "root"; 
+$password = ""; 
+$dbname = "smartlib"; 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
