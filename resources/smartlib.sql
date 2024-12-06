5845ee740b435175d55bb122751efd4bff3e83d4
M.M.F.Najma <s92082845@ousl.lk>
2024-12-06 21:36:48 +0530

    Update smartlib.sql

---
+++ resources/smartlib.sql
@@ -0,0 +1,147 @@
+/*
+SQLyog Community
+MySQL - 10.4.28-MariaDB : Database - smartlib
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`smartlib` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `smartlib`;
+
+/*Table structure for table `book_loans` */
+
+DROP TABLE IF EXISTS `book_loans`;
+
+CREATE TABLE `book_loans` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `book_id` int(11) DEFAULT NULL,
+  `member_id` int(11) DEFAULT NULL,
+  `loan_date` date DEFAULT NULL,
+  `return_date` date DEFAULT NULL,
+  `is_return` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+/*Data for the table `book_loans` */
+
+/*Table structure for table `books` */
+
+DROP TABLE IF EXISTS `books`;
+
+CREATE TABLE `books` (
+  `guid` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `publication_year` varchar(10) DEFAULT NULL,
+  `isbn` varchar(255) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+/*Data for the table `books` */
+
+/*Table structure for table `categories` */
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `guiid` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+/*Data for the table `categories` */
+
+insert  into `categories`(`guiid`,`id`,`name`,`created_at`,`updated_at`) values 
+('',3,'Fiction','2024-09-30 21:59:37',NULL),
+('',4,'Non-Fiction','2024-09-30 21:59:37',NULL),
+('',5,'Science                                                            \r\n','2024-09-30 22:00:39',NULL),
+('',6,'Technology','2024-09-30 22:00:39',NULL);
+
+/*Table structure for table `subscription_plans` */
+
+DROP TABLE IF EXISTS `subscription_plans`;
+
+CREATE TABLE `subscription_plans` (
+  `guid` varchar(50) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `amount` float(10,2) NOT NULL DEFAULT 0.00,
+  `duration` int(11) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+/*Data for the table `subscription_plans` */
+
+/*Table structure for table `subscriptions` */
+
+DROP TABLE IF EXISTS `subscriptions`;
+
+CREATE TABLE `subscriptions` (
+  `guid` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `member_id` int(11) DEFAULT NULL,
+  `plan_id` int(11) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `amount` float(10,2) DEFAULT NULL,
+  `is_expired` bit(1) DEFAULT b'0',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+/*Data for the table `subscriptions` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `guid` varchar(50) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `nic_no` varchar(50) NOT NULL,
+  `phone_no` varchar(20) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `profile_pic` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `is_admin` bit(1) DEFAULT b'0',
+  `reset_code` varchar(50) DEFAULT NULL,
+  `reset_at` timestamp NULL DEFAULT NULL,
+  `is_blocked` bit(1) DEFAULT b'0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`guid`,`id`,`name`,`email`,`nic_no`,`phone_no`,`address`,`password`,`profile_pic`,`created_at`,`updated_at`,`is_admin`,`reset_code`,`reset_at`,`is_blocked`) values 
+('6F3F4017-5D77-4367-AB8E-9C9564DB7676',5,'Ishthiyaque Ahmed','ishthi83@gmail.com','831780630V','0715308889','No 45','c2ed9df03d80eac8f392b27548f20851',NULL,'2024-10-24 22:55:11','2024-10-24 23:23:34','',NULL,NULL,'\0');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
