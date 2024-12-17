5147c1a15786495197eb6530ccc5e0d6d7512ea8
fathimashamee <s92062674@ousl.lk>
2024-12-17 15:53:35 +0530

    Update changes in reviewed-rated-books-UI

--- admin/search-book/reviewed-rated-books.php
+++ admin/search-book/reviewed-rated-books.php
@@ -8,11 +8,23 @@ include_once("../../include/sidebar.php");
 <!-- Main Content Start -->
 <main class="mt-1 pt-3">
 
-    <div class="container mt-4">
-        <h3 class="text-center">Reviewed & Rated Books</h3>
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped">
-                <thead class="bg-primary text-white">
+    <div class="container-fluid">
+        <!-- Manage Multimedia Room Bookings Section -->
+        <div class="row">
+            <div class="col-md-12">
+                <h4 class="fw-bold text-uppercase">Reviewed & Rated Books</h4>
+    </div>
+
+    <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Rated & Reviewed Books Overview
+                    </div>
+
+                    <div class="card-body">
+                    <div class="table-responsive">
+            <table class="table table-bordered table-striped" id="reviewed&ratedTable">
+                <thead class="table-dark">
                     <tr>
                         <th>#</th>
                         <th>Book Title</th>
